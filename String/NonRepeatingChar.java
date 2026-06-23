e93454be7dedbf93d49555bfb7d67fea0187132e
hiteshSingh57 <hiteshsingh001361@gmail.com>
2026-06-23 12:07:05 +0530

    Check Non Reapeting Charachter

--- String/NonRepeatingChar.java
+++ String/NonRepeatingChar.java
@@ -1,18 +1,22 @@
 public class NonRepeatingChar {
-    public String non_repeated( String str) {
+    public char non_repeated(String str) {
         for(int i = 0;i<str.length();i++) {
             int count=0;
-            for(int j = 0;j<str.length();j++) {
-                if(str.charAt(j) == str.charAt(i));
-                count += 1;
+            for(int j = 0; j < str.length(); j++) {
+                if(str.charAt(j) == str.charAt(i)) {
+                    count++;
+                }
+            }
+            if(count == 1) {
+                return str.charAt(i);   
             }
         }
-        return ' ';
+         return ' ';
     }
     public static void main(String[] args) {
-        String sttr = "This is a statemenet";
-        int result = non_repeated(str);
+        String str = "aabbcd";
+        NonRepeatingChar obj = new NonRepeatingChar();
+        char result = obj.non_repeated(str);
         System.out.println(result);
     }
-    
 }
