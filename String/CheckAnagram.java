526413ba66c981b627ab7103de10c899281a24ee
hiteshSingh57 <hiteshsingh001361@gmail.com>
2026-06-23 12:49:45 +0530

    Check Anagram

---
+++ String/CheckAnagram.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+public class CheckAnagram {
+    public static boolean isAnagram(String str1, String str2) {
+        if(str1.length() != str2.length()) {
+            return false;
+        }
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent"));
+        System.out.println(isAnagram("hello", "world"));
+    }
+    
+}
